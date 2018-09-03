281a6f49d71650418504d28078ebda3d5fa7f498
nwalter <walter.nicolas@gmx.de>
2018-09-03 22:49:47 +0200

    beliebige anzahl am stück erraten

--- src/public/js/Other/init.ts
+++ src/public/js/Other/init.ts
@@ -1,47 +1,83 @@
 function next(
-    valueList: string[],
+    hiragana: string[],
+    katakana: string[],
     inputHira: HTMLInputElement,
     inputKata: HTMLInputElement,
     jpnLabel: HTMLElement,
-    lastIndex: number
-): [string, number] {
+    lastIndex: number[],
+    count: HTMLInputElement
+): [string[], number[]] {
     let random: number;
-    do {
-        random = Math.round(Math.random() * (valueList.length-1));
-    } while (random === lastIndex);
-    jpnLabel.innerHTML = valueList[random];
+    let randomList: number[] = [];
+    let strList: string[] = [];
+    let str: string = '';
+    let valueList: string[];
+    if (Math.round(Math.random()) === 1) {
+        valueList = hiragana;
+    } else {
+        valueList = katakana;
+    }
+    for (let i = 0; i < parseInt(count.value); i++) {
+        do {
+            random = Math.round(Math.random() * (valueList.length - 1));
+        } while (random === lastIndex[i]);
+        str += valueList[random];
+        randomList[i] = random;
+        strList[i] = valueList[random];
+    }
+    jpnLabel.innerHTML = str;
     inputHira.value = '';
     inputKata.value = '';
     inputHira.classList.remove('wrong-answer');
     inputKata.classList.remove('wrong-answer');
-    return [valueList[random], random];
+    return [strList, randomList];
 }
 
 function getAllValues(res: any) {
     const list = [];
-    for(let style in res) {
-        if(res.hasOwnProperty(style)) {
-            for(let key in res[style]) {
-                if(res[style].hasOwnProperty(key)) {
-                    list.push(res[style][key]);
-                }
-            }
+    for (let key in res) {
+        if (res.hasOwnProperty(key)) {
+            list.push(res[key]);
         }
     }
     return list;
 }
 
-function getSolution(res: any, actualChar: string) {
-    for(let key in res.hiragana) {
-        if(res.hiragana.hasOwnProperty(key) && res.hiragana[key] === actualChar) {
-            return [key, 'hiragana'];
+function compare(input: string, actual: string[]) {
+    const list = input.split(' ');
+    if (list.length !== actual.length) {
+        return false;
+    }
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] !== actual[i]) {
+            return false;
         }
     }
-    for(let key in res.katakana) {
-        if(res.katakana.hasOwnProperty(key) && res.katakana[key] === actualChar) {
-            return [key, 'katakana'];
+    return true;
+}
+
+function getSolution(res: any, actualList: string[]): [string[], string] {
+    const keyList: string[] = [];
+    for (let i = 0; i < actualList.length; i++) {
+        for (let key in res.hiragana) {
+            if (res.hiragana.hasOwnProperty(key) && res.hiragana[key] === actualList[i]) {
+                keyList.push(key);
+                break;
+            }
         }
     }
+    if (keyList.length !== 0) {
+        return [keyList, 'hiragana'];
+    }
+    for (let i = 0; i < actualList.length; i++) {
+        for (let key in res.katakana) {
+            if (res.katakana.hasOwnProperty(key) && res.katakana[key] === actualList[i]) {
+                keyList.push(key);
+                break;
+            }
+        }
+    }
+    return [keyList, 'katakana'];
 }
 
 function init() {
@@ -49,47 +85,47 @@ function init() {
     const inputHira: any = document.getElementById('input-hiragana');
     const inputKata: any = document.getElementById('input-katakana');
     const inputRoman: any = document.getElementById('input-romanji');
+    const count: any = document.getElementById('count-chars');
     const jpnLabel = document.getElementById('japanese-char');
     AjaxRequest.sendAjaxRequest('../api/get.php', null, function (http) {
         AjaxRequest.errFunction(http, 'get');
     }, (http) => {
         const res = JSON.parse(http.response);
         console.log(res);
-        if(res.hiragana === null || res.katakana === null) {
+        if (res.hiragana === null || res.katakana === null) {
             return;
         }
-        const valueList = getAllValues(res);
-        let [actualChar, lastIndex] = next(valueList, inputHira, inputKata, jpnLabel, -1);
+        let [actualList, lastIndexList] = next(getAllValues(res.hiragana), getAllValues(res.katakana), inputHira, inputKata, jpnLabel, [-1], count);
         inputHira.addEventListener('keydown', function (ev: any) {
-            if(ev.keyCode === 39) {
+            if (ev.keyCode === 39) {
                 inputKata.focus();
             }
-            if(ev.key !== 'Enter') {
+            if (ev.key !== 'Enter') {
                 return;
             }
-            if(res.hiragana[inputHira.value] !== actualChar) {
+            if (!compare(inputHira.value, getSolution(res, actualList)[0])) {
                 inputHira.classList.add('wrong-answer');
                 inputHira.value = '';
                 return;
             }
-            [actualChar, lastIndex] = next(valueList, inputHira, inputKata, jpnLabel, lastIndex);
+            [actualList, lastIndexList] = next(getAllValues(res.hiragana), getAllValues(res.katakana), inputHira, inputKata, jpnLabel, lastIndexList, count);
         });
         inputKata.addEventListener('keydown', function (ev: any) {
-            if(ev.keyCode === 37) {
+            if (ev.keyCode === 37) {
                 inputHira.focus();
             }
-            if(ev.key !== 'Enter') {
+            if (ev.key !== 'Enter') {
                 return;
             }
-            if(res.katakana[inputKata.value] !== actualChar) {
+            if (!compare(inputKata.value, getSolution(res, actualList)[0])) {
                 inputKata.classList.add('wrong-answer');
                 inputKata.value = '';
                 return;
             }
-            [actualChar, lastIndex] = next(valueList, inputHira, inputKata, jpnLabel, lastIndex);
+            [actualList, lastIndexList] = next(getAllValues(res.hiragana), getAllValues(res.katakana), inputHira, inputKata, jpnLabel, lastIndexList, count);
         });
         inputRoman.addEventListener('keydown', function (ev: any) {
-            if(ev.key !== 'Enter') {
+            if (ev.key !== 'Enter') {
                 return;
             }
             jpnLabel.innerHTML = res.hiragana[inputRoman.value];
@@ -97,16 +133,27 @@ function init() {
             inputRoman.value = '';
         });
         button.addEventListener('click', function () {
-            const [solution, style] = getSolution(res, actualChar);
-            if(style === 'katakana') {
-                inputKata.value = solution;
+            const [solution, style] = getSolution(res, actualList);
+            if (style === 'katakana') {
+                inputKata.value = solutionToString(solution);
                 inputKata.focus();
             } else {
-                inputHira.value = solution;
+                inputHira.value = solutionToString(solution);
                 inputHira.focus();
             }
         });
     });
+}
+
+function solutionToString(keyList: string[]) {
+    let str = '';
+    for (let i = 0; i < keyList.length; i++) {
+        str += keyList[i];
+        if (i !== keyList.length - 1) {
+            str += ' ';
+        }
+    }
+    return str;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
--- src/public/js/polyfills.js
+++ src/public/js/polyfills.js
@@ -37,48 +37,83 @@ var AjaxRequest = /** @class */ (function () {
     return AjaxRequest;
 }());
 //# sourceMappingURL=AjaxRequest.js.map
-function next(valueList, inputHira, inputKata, jpnLabel, lastIndex) {
+function next(hiragana, katakana, inputHira, inputKata, jpnLabel, lastIndex, count) {
     var random;
-    do {
-        random = Math.round(Math.random() * (valueList.length - 1));
-    } while (random === lastIndex);
-    jpnLabel.innerHTML = valueList[random];
+    var randomList = [];
+    var strList = [];
+    var str = '';
+    var valueList;
+    if (Math.round(Math.random()) === 1) {
+        valueList = hiragana;
+    }
+    else {
+        valueList = katakana;
+    }
+    for (var i = 0; i < parseInt(count.value); i++) {
+        do {
+            random = Math.round(Math.random() * (valueList.length - 1));
+        } while (random === lastIndex[i]);
+        str += valueList[random];
+        randomList[i] = random;
+        strList[i] = valueList[random];
+    }
+    jpnLabel.innerHTML = str;
     inputHira.value = '';
     inputKata.value = '';
     inputHira.classList.remove('wrong-answer');
     inputKata.classList.remove('wrong-answer');
-    return [valueList[random], random];
+    return [strList, randomList];
 }
 function getAllValues(res) {
     var list = [];
-    for (var style in res) {
-        if (res.hasOwnProperty(style)) {
-            for (var key in res[style]) {
-                if (res[style].hasOwnProperty(key)) {
-                    list.push(res[style][key]);
-                }
-            }
+    for (var key in res) {
+        if (res.hasOwnProperty(key)) {
+            list.push(res[key]);
         }
     }
     return list;
 }
-function getSolution(res, actualChar) {
-    for (var key in res.hiragana) {
-        if (res.hiragana.hasOwnProperty(key) && res.hiragana[key] === actualChar) {
-            return [key, 'hiragana'];
+function compare(input, actual) {
+    var list = input.split(' ');
+    if (list.length !== actual.length) {
+        return false;
+    }
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] !== actual[i]) {
+            return false;
         }
     }
-    for (var key in res.katakana) {
-        if (res.katakana.hasOwnProperty(key) && res.katakana[key] === actualChar) {
-            return [key, 'katakana'];
+    return true;
+}
+function getSolution(res, actualList) {
+    var keyList = [];
+    for (var i = 0; i < actualList.length; i++) {
+        for (var key in res.hiragana) {
+            if (res.hiragana.hasOwnProperty(key) && res.hiragana[key] === actualList[i]) {
+                keyList.push(key);
+                break;
+            }
         }
     }
+    if (keyList.length !== 0) {
+        return [keyList, 'hiragana'];
+    }
+    for (var i = 0; i < actualList.length; i++) {
+        for (var key in res.katakana) {
+            if (res.katakana.hasOwnProperty(key) && res.katakana[key] === actualList[i]) {
+                keyList.push(key);
+                break;
+            }
+        }
+    }
+    return [keyList, 'katakana'];
 }
 function init() {
     var button = document.getElementById('button-help');
     var inputHira = document.getElementById('input-hiragana');
     var inputKata = document.getElementById('input-katakana');
     var inputRoman = document.getElementById('input-romanji');
+    var count = document.getElementById('count-chars');
     var jpnLabel = document.getElementById('japanese-char');
     AjaxRequest.sendAjaxRequest('../api/get.php', null, function (http) {
         AjaxRequest.errFunction(http, 'get');
@@ -88,8 +123,7 @@ function init() {
         if (res.hiragana === null || res.katakana === null) {
             return;
         }
-        var valueList = getAllValues(res);
-        var _a = next(valueList, inputHira, inputKata, jpnLabel, -1), actualChar = _a[0], lastIndex = _a[1];
+        var _a = next(getAllValues(res.hiragana), getAllValues(res.katakana), inputHira, inputKata, jpnLabel, [-1], count), actualList = _a[0], lastIndexList = _a[1];
         inputHira.addEventListener('keydown', function (ev) {
             if (ev.keyCode === 39) {
                 inputKata.focus();
@@ -97,12 +131,12 @@ function init() {
             if (ev.key !== 'Enter') {
                 return;
             }
-            if (res.hiragana[inputHira.value] !== actualChar) {
+            if (!compare(inputHira.value, getSolution(res, actualList)[0])) {
                 inputHira.classList.add('wrong-answer');
                 inputHira.value = '';
                 return;
             }
-            _a = next(valueList, inputHira, inputKata, jpnLabel, lastIndex), actualChar = _a[0], lastIndex = _a[1];
+            _a = next(getAllValues(res.hiragana), getAllValues(res.katakana), inputHira, inputKata, jpnLabel, lastIndexList, count), actualList = _a[0], lastIndexList = _a[1];
             var _a;
         });
         inputKata.addEventListener('keydown', function (ev) {
@@ -112,12 +146,12 @@ function init() {
             if (ev.key !== 'Enter') {
                 return;
             }
-            if (res.katakana[inputKata.value] !== actualChar) {
+            if (!compare(inputKata.value, getSolution(res, actualList)[0])) {
                 inputKata.classList.add('wrong-answer');
                 inputKata.value = '';
                 return;
             }
-            _a = next(valueList, inputHira, inputKata, jpnLabel, lastIndex), actualChar = _a[0], lastIndex = _a[1];
+            _a = next(getAllValues(res.hiragana), getAllValues(res.katakana), inputHira, inputKata, jpnLabel, lastIndexList, count), actualList = _a[0], lastIndexList = _a[1];
             var _a;
         });
         inputRoman.addEventListener('keydown', function (ev) {
@@ -129,17 +163,27 @@ function init() {
             inputRoman.value = '';
         });
         button.addEventListener('click', function () {
-            var _a = getSolution(res, actualChar), solution = _a[0], style = _a[1];
+            var _a = getSolution(res, actualList), solution = _a[0], style = _a[1];
             if (style === 'katakana') {
-                inputKata.value = solution;
+                inputKata.value = solutionToString(solution);
                 inputKata.focus();
             }
             else {
-                inputHira.value = solution;
+                inputHira.value = solutionToString(solution);
                 inputHira.focus();
             }
         });
     });
+}
+function solutionToString(keyList) {
+    var str = '';
+    for (var i = 0; i < keyList.length; i++) {
+        str += keyList[i];
+        if (i !== keyList.length - 1) {
+            str += ' ';
+        }
+    }
+    return str;
 }
 document.addEventListener('DOMContentLoaded', function () {
     init();
